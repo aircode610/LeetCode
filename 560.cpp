7ef1321c8a0beb98ea721974e958e4773cacd67b
Amirali Iranmanesh <34022796+aircode610@users.noreply.github.com>
2025-10-14 18:42:35 +0200

    Solved 560. Subarray Sum Equals K

---
+++ 560.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int sum = 0, ans = 0;
+        unordered_map<int, int> pref;
+        pref[0] = 1;
+        for (int num : nums) {
+            sum += num;
+            ans += pref[sum - k];
+            pref[sum]++;
+        }
+
+        return ans;
+    }
+};
